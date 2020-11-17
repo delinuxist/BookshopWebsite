cba80bffd9930aecd85921ee50dcc7524b19b6c6
delinuxist <delinuxist@gmail.com>
2020-11-17 09:00:30 +0000

    famous writers

--- js/app.js
+++ js/app.js
@@ -11,4 +11,17 @@ $(document).ready(function () {
       $(".gallery").removeClass("change");
     }
   });
+
+  $(".writers-accordion").click(function (event) {
+    if (event.target.id.split("-")[0] === "button") {
+      $("#book1").attr(
+        "src",
+        "/img/writers/" + event.target.id.split("-")[1] + "-book1.jpg"
+      );
+      $("#book2").attr(
+        "src",
+        "/img/writers/" + event.target.id.split("-")[1] + "-book2.jpg"
+      );
+    }
+  });
 });
